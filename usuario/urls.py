a25111c3858739f8802cf87026dd22bceb8266e7
Rafael Romario <romarovin11@gmail.com>
2017-06-16 12:12:58 -0500

    Creacion de vistas de perfiles evitgando que si ya existe el perfil se pueda volver a crear(ocultando),crear y Editar

--- usuario/urls.py
+++ usuario/urls.py
@@ -4,17 +4,17 @@ from django.views.generic import TemplateView
 #from recursos import views
 from django.contrib.auth import views as auth_views
 from django.views.generic import TemplateView
-urlpatterns = [#cambiar url logout
-		url(r'^logout$', auth_views.logout, {'next_page': '/'}, name="logout"),
-    url(r'^$', TemplateView.as_view(template_name="index.html"), name="index"),
-    url(r'^plataforma', TemplateView.as_view(template_name="system_index.html"), name="system_index"),
-    url(r'^login/$', views.authentication, name="authentication"),
-    #url(r'^login', TemplateView.as_view(template_name="login.html"), name="login"),
-   # url(r'^plataforma', views.ResourceInsert.as_view(), name="plataforma"),
 
-    #url(r'^', views.hello, name="hello"),
-     #   url(r'^logout$', auth_views.logout, {'next_page': '/'}, name="logout"),
-    #url(r'^login/$', views.authentication, name="authentication"),
+urlpatterns = [#cambiar url logout
+	url(r'^logout/$', auth_views.logout, {'next_page': '/'}, name="logout"),
+    url(r'^$', TemplateView.as_view(template_name="index.html"), name="index"),
+    url(r'^login/$', views.authentication, name="authentication"),
+    url(r'^plataforma', TemplateView.as_view(template_name="system_index.html"), name="system_index"),
+    url(r'^perfil/new', views.PerfilInsert.as_view(), name="perfil_insert"),
+    url(r'^perfil/list$',views.PerfilList.as_view(), name='perfil_list'),
+    url(r'^perfil/edit(?P<pk>[0-9]+)/$',views.PerfilUpdate.as_view(), 
+    	name='perfil_edit'),
+    
 
 
 ]
--- usuario/views.py
+++ usuario/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login
+#from django.contrib.auth import logout
+from .models import Perfil
+
+#Vistas 
+from django.views.generic import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import (
+    UpdateView, 
+    CreateView, 
+    DeleteView
+    )
+from django.core.urlresolvers import reverse_lazy
 
 
 def authentication(request):
@@ -11,18 +23,48 @@ def authentication(request):
         print(username)
         print(password)
         if action == 'login':
-            user = authenticate(username=username, password=password)
-            
+            user = authenticate(username=username, password=password)          
             if user is not None:
             	login(request, user)
                 return redirect('usuario:system_index')
         return redirect('usuario:index')
-
-
-
     return render(request, 'login.html', {})
 
-"""
-def hello(request):
-	return render(request, 'hello.html')
-"""
+
+#-----------------------Perfil------------------------#
+class PerfilInsert(CreateView):
+    permission_required = ('usuario.add_perfil')
+    model = Perfil
+    success_url = reverse_lazy('usuario:system_index')
+    fields = ['numeroDocumento','telefono','direccion',]
+    success_url = reverse_lazy('usuario:system_index')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        #form.user.groups.add('Usuario_Standard','Administrador_Centuria')
+                #aca
+        #user.groups.add('Usuario_Standard','Administrador_Centuria')
+        return super(PerfilInsert, self).form_valid(form)
+
+
+class PerfilList(ListView):#, 
+#    PermissionAdminRequiredMixin, PermissionStandardRequiredMixin):
+    model = Perfil
+    context_object_name = 'perfiles'
+#esta funcion permite ver solo las reservaciones del usuario
+    def get_queryset(self):
+        return Perfil.objects.filter(user=self.request.user)
+
+
+#editar perfil actual
+class PerfilUpdate(UpdateView):#,
+#    PermissionAdminRequiredMixin, PermissionStandardRequiredMixin):
+    model = Perfil
+    success_url = reverse_lazy('usuario:system_index')
+    fields = ['numeroDocumento','telefono','direccion',] 
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(PerfilUpdate, self).form_valid(form)
+#-----------------END - Profile------------------
+
