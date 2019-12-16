d441278494bde9ede004ee5b8a3117716b51e2a1
tom-sb <fvillanuevanut@gmail.com>
2019-12-15 21:34:46 -0500

    commit de prueba
    Co-authored-by: tom-sb<fvillanuevanut@gmail.com>

--- usuario/tests.py
+++ usuario/tests.py
@@ -1,8 +1,8 @@
 from django.test import TestCase
 #from .models import Perfil
 
-#class PerfilTestCase(TestCase):
-#	def setUp(self):
-#		self.usuario = Perfil()
+class PerfilTestCase(TestCase):
+	def setUp(self):
+		self.usuario = Perfil()
         
 # Create your tests here.
