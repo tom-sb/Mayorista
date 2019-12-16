1392f98325c8e3a341ec9bc93cd08bc61099d331
Fabricio <zap.xtp@gmail.com>
2019-12-15 21:29:55 -0500

    test bassico2

--- cliente/tests.py
+++ cliente/tests.py
@@ -3,13 +3,12 @@ from django.test import TestCase
 # Create your tests here.
 class BasicTest(TestCase):
     def test_fields(self):
-        item = Item()
-        item.TIPO_CUENTA = "Make more cuentas"
-        item.nombre = "here name"
-        item.save()
-  """      item = Item,objects.all()
+        cliente = Cliente()
+        cliente.TIPO_CUENTA = "Make more cuentas"
+        cliente.nombre = "here name"
+        cliente.save()
+"""      item = Item,objects.all()
         self.assertEquals(item.count(), 1)"""
-        
-        record = Item.objects.get(pk1)
+        record = Cliente.objects.get(pk1)
         self.assertEqual(record, item)
     
